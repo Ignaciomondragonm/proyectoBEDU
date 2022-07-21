2a9de0e100ec9e0cb2710ae4ad72b0b541b21868
Ignacio Mondragon <ignaciomondragonm@gmail.com>
2022-07-21 17:50:09 -0500

    M3 BackEnd 6

--- Procesamiento_datos_Python/Backend/index.js
+++ Procesamiento_datos_Python/Backend/index.js
@@ -13,15 +13,10 @@ const app = express();
 app.use( cors() );
 app.use( express.json() );
 
-// // Lectura y parseo del body
-// app.use(express.json({limit: '50mb'}));
-// app.use(express.urlencoded({limit: '50mb'}));
-
 // Base de datos
 dbConnection();
 
 app.use( '/api/craigslists', require('./routes/craigslists') );
-app.use( '/api/ventas', require('./routes/ventas') );
 
 
 
